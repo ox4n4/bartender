429c27d4fc61cda1f649df9342b1ea08d2af23aa
ox4n4 <oxana.pilenko@gmail.com>
2020-06-22 19:44:30 -0400

    made some changes to my doc

--- barguide.py
+++ barguide.py
@@ -6,6 +6,7 @@ headers = { 'Content-Type': 'application/json'}
 def get_cocktail():
     api_url_base = 'https://www.thecocktaildb.com/api/json/v2/9973553/popular.php'
     api_token = '9973553'
+
     response = requests.get(api_url, headers=headers)
 
     if response.status.code == 200:
